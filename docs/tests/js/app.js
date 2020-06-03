8ab4fa37126f7649c17dc6c9dbcd4a2277eae4b5
AlexMakdis <alexmakd@student.arteveldehs.be>
2020-06-03 16:03:20 +0200

    test

--- docs/tests/js/app.js
+++ docs/tests/js/app.js
@@ -1,5 +1,5 @@
 (() => {  
-  const DOMAIN = 'https://github.com/AlexMakdis/eindwerk-baas-AlexMakdis';
+  const DOMAIN = 'https://github.com/AlexMakdis/eindwerk-baas-AlexMakdis/docs';
 
   const loadBlog = async () => {
     const response = await fetch(`${DOMAIN}/data/hotSauce/index.json`);
