924dcbcb1c7013386f6e0f0bfa1204c6022a42ca
AlexMakdis <alexmakd@student.arteveldehs.be>
2020-06-10 19:15:41 +0200

    reviews

---
+++ docs/data/reviews/index.js
@@ -0,0 +1,63 @@
+[{
+    "id": "0",
+    "name": "Alex Makdis Afrim",
+    "text": "Super goeie sauzen!",
+    "date": "10 juni 2020",
+    "stars": "5"
+},{
+    "id": "1",
+    "name": "Piet Huysentruyt",
+    "text": "Wat ik vandaag zullen leren is dat deze sauzen heerlijk zijn!",
+    "date": "5 januari 2020",
+    "stars": "4"
+},{
+    "id": "2",
+    "name": "Jeroen Meus",
+    "text": "Nieuwe sauzen gevonden voor de Dagelijkse Kost. Sommige...",
+    "date": "8 mei 2020",
+    "stars": "3"
+},{
+    "id": "3",
+    "name": "Philippe Geubels",
+    "text": "Door deze sauzen praat kik rapper..",
+    "date": "15 maart 2020",
+    "stars": "5"
+},{
+    "id": "4",
+    "name": "Gert verhulst",
+    "text": "Deze saus zo pikant! Past perfect in Plopsaland",
+    "date": "25 april 2020",
+    "stars": "3"
+},{
+    "id": "5",
+    "name": "Balthazar Boma",
+    "text": "Mijn gedacht!",
+    "date": "2 februari 2020",
+    "stars": "3"
+},{
+    "id": "6",
+    "name": "Xavier Waterslaeghers",
+    "text": "Past bij een goeien dagschotel!",
+    "date": "9 april 2020",
+    "stars": "4"
+},{
+    "id": "7",
+    "name": "Marc Vertongen",
+    "text": "Veel te pikant!",
+    "date": "20 januari 2020",
+    "stars": "1"
+},{
+    "id": "8",
+    "name": "William Boeva",
+    "text": "Ik heb gehoord dat pikante sauzen helpen voor de groei... Helaas.",
+    "date": "5 mei 2020",
+    "stars": "2"
+},{
+    "id": "9",
+    "name": "Sergio Herman",
+    "text": "Goedgekeurd",
+    "date": "18 april 2020",
+    "stars": "5"
+}
+
+]
